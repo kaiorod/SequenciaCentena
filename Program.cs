3c15725fdca274dbb13813f04e89d6b7c7d3283a
kaiorod <caiorodrigo2775@gmail.com>
2021-11-12 21:56:38 -0300

    Finaliza SequenciaCentena

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+
+namespace SequenciaCentena
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int c = 1;
+            Console.Clear();
+            Console.Beep();
+            while(c <= 100)
+            {
+                Thread.Sleep(50);
+                Console.Write($" {c}");
+                c++;
+            }
+            Console.WriteLine("");
+        }
+    }
+}
